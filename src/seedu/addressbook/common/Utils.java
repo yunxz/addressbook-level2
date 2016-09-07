f8f132782b911735ae6e2a2b3f0ea0b09124a758
yunxz <ih.c.frv@gmail.com>
2016-09-07 23:16:25 +0800

    T4A4

--- src/seedu/addressbook/common/Utils.java
+++ src/seedu/addressbook/common/Utils.java
@@ -5,34 +5,48 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import seedu.addressbook.data.person.Printable;
+
 /**
  * Utility methods
  */
 public class Utils {
 
-    /**
-     * Checks whether any of the given items are null.
-     */
-    public static boolean isAnyNull(Object... items) {
-        for (Object item : items) {
-            if (item == null) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Checks whether any of the given items are null.
+	 */
+	public static boolean isAnyNull(Object... items) {
+		for (Object item : items) {
+			if (item == null) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Checks if every element in a collection are unique by {@link Object#equals(Object)}.
-     */
-    public static boolean elementsAreUnique(Collection<?> items) {
-        final Set<Object> testSet = new HashSet<>();
-        for (Object item : items) {
-            final boolean itemAlreadyExists = !testSet.add(item); // see Set documentation
-            if (itemAlreadyExists) {
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * Checks if every element in a collection are unique by
+	 * {@link Object#equals(Object)}.
+	 */
+	public static boolean elementsAreUnique(Collection<?> items) {
+		final Set<Object> testSet = new HashSet<>();
+		for (Object item : items) {
+			final boolean itemAlreadyExists = !testSet.add(item); // see Set
+																	// documentation
+			if (itemAlreadyExists) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Returns a concatenated version of the printable strings of each object.
+	 */
+	String getPrintableString(Printable... printables) {
+		StringBuilder destination = new StringBuilder();
+		for(Printable p : printables)
+			destination.append(p.getPrintableString());
+	    return destination.toString();
+	}
 }
